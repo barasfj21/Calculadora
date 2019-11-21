d248347beeaabc8219a8f12f7ecf8ee0d91b238e
Froyd Marchena <marchena.froyd23@gmail.com>
2019-11-21 17:22:56 -0600

    XOR

--- Compuertas.java
+++ Compuertas.java
@@ -4,7 +4,7 @@ public class Compuertas {
 		return !a;
 	}
 	public Boolean NAND(Boolean a,Boolean b,Boolean c,Boolean d) {
-		return a&b&c&d;
+		return !(a&b&c&d);
 	}
 	public Boolean AND(Boolean a,Boolean b,Boolean c,Boolean d) {
 		return NOT(NAND(a,b,c,d));
@@ -15,9 +15,9 @@ public class Compuertas {
 	public Boolean NOR(Boolean a,Boolean b,Boolean c,Boolean d) {
 		return NOT(OR(a,b,c,d));
 	}
-	public Boolean XOR(Boolean a,Boolean b,Boolean c,Boolean d) {
-		//return NAND(NAND(a,),NAND(),NAND());
-	}
+	public static Boolean XOR(Boolean a,Boolean b) {
+		return (NAND(NAND(a,NAND(b,a,true,true),true,true),NAND(b,NAND(b,a,true,true),true,true),true,true));
+    }
 	public static void main(String []args) {
 		Compuertas asd = new Compuertas();
 		System.out.println(asd.NOT(true));
