931bd5e4a38eab47622ec51376e8b99f38a6b81c
SopaInstantanea <49882187+SopaInstantanea@users.noreply.github.com>
2019-11-21 16:09:49 -0600

    asd

--- Compuertas.java
+++ Compuertas.java
@@ -6,6 +6,18 @@ public class Compuertas {
 	public Boolean NAND(Boolean a,Boolean b,Boolean c) {
 		return a&b&c;
 	}
+	public Boolean AND(Boolean a,Boolean b,Boolean c) {
+		return NOT(NAND(a,b,c));
+	}
+	public Boolean OR(Boolean a,Boolean b,Boolean c) {
+		return NAND(NOT(a),NOT(b),NOT(c));
+	}
+	public Boolean NOR(Boolean a,Boolean b,Boolean c) {
+		return NOT(OR(a,b,c));
+	}
+	public Boolean XOR(Boolean a,Boolean b,Boolean c) {
+		//return NAND(NAND(a,),NAND(),NAND());
+	}
 	public static void main(String []args) {
 		Compuertas asd = new Compuertas();
 		System.out.println(asd.NOT(true));
