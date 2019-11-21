0729fe8b456b4fd4d87a1800be85288a2556cdb7
SopaInstantanea <49882187+SopaInstantanea@users.noreply.github.com>
2019-11-21 16:18:14 -0600

    asdasd

--- Compuertas.java
+++ Compuertas.java
@@ -3,24 +3,24 @@ public class Compuertas {
 	public Boolean NOT(Boolean a) {
 		return !a;
 	}
-	public Boolean NAND(Boolean a,Boolean b,Boolean c) {
-		return a&b&c;
+	public Boolean NAND(Boolean a,Boolean b,Boolean c,Boolean d) {
+		return a&b&c&d;
 	}
-	public Boolean AND(Boolean a,Boolean b,Boolean c) {
-		return NOT(NAND(a,b,c));
+	public Boolean AND(Boolean a,Boolean b,Boolean c,Boolean d) {
+		return NOT(NAND(a,b,c,d));
 	}
-	public Boolean OR(Boolean a,Boolean b,Boolean c) {
-		return NAND(NOT(a),NOT(b),NOT(c));
+	public Boolean OR(Boolean a,Boolean b,Boolean c,Boolean d) {
+		return NAND(NOT(a),NOT(b),NOT(c),NOT(d));
 	}
-	public Boolean NOR(Boolean a,Boolean b,Boolean c) {
-		return NOT(OR(a,b,c));
+	public Boolean NOR(Boolean a,Boolean b,Boolean c,Boolean d) {
+		return NOT(OR(a,b,c,d));
 	}
-	public Boolean XOR(Boolean a,Boolean b,Boolean c) {
+	public Boolean XOR(Boolean a,Boolean b,Boolean c,Boolean d) {
 		//return NAND(NAND(a,),NAND(),NAND());
 	}
 	public static void main(String []args) {
 		Compuertas asd = new Compuertas();
 		System.out.println(asd.NOT(true));
-		System.out.println(asd.NAND(true,true,true));
+		System.out.println(asd.NAND(true,true,true,true));
 	}
 }
