fc17882a2ef1e0a40b2b9778a54e145791cc5b4b
SopaInstantanea <49882187+SopaInstantanea@users.noreply.github.com>
2019-11-21 15:52:32 -0600

    cambios

--- Compuertas.java
+++ Compuertas.java
@@ -1,7 +1,14 @@
 
 public class Compuertas {
-	public Boolean NOT(Boolean a,Boolean b,Boolean c) {
-			
-		return false;
+	public Boolean NOT(Boolean a) {
+		return !a;
+	}
+	public Boolean NAND(Boolean a,Boolean b,Boolean c) {
+		return a&b&c;
+	}
+	public static void main(String []args) {
+		Compuertas asd = new Compuertas();
+		System.out.println(asd.NOT(true));
+		System.out.println(asd.NAND(true,true,true));
 	}
 }
