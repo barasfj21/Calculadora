95f42b4ca12b4279f2976b1a5457dda017358096
SopaInstantanea <49882187+SopaInstantanea@users.noreply.github.com>
2019-11-21 18:24:36 -0600

    alu lista

--- Compuertas.java
+++ Compuertas.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class Compuertas {
 	public Boolean NOT(Boolean a) {
@@ -17,10 +19,30 @@ public class Compuertas {
 	}
 	public Boolean XOR(Boolean a,Boolean b) {
 		return (NAND(NAND(a,NAND(b,a,true,true),true,true),NAND(b,NAND(b,a,true,true),true,true),true,true));
-    }
+	}
+	
+	public ArrayList<Boolean> Alu(Boolean a1,Boolean a2,Boolean a3,Boolean a4,Boolean SR,Boolean b1,Boolean b2,Boolean b3,Boolean b4) {
+		ArrayList<Boolean> listaRespuesta = new ArrayList<Boolean>();
+		Boolean[] respuestas = sumador(a4,XOR(SR,b4),SR);
+		listaRespuesta.add(respuestas[1]);
+		respuestas = sumador(a3,XOR(SR,b3),respuestas[0]);
+		listaRespuesta.add(respuestas[1]);
+		respuestas = sumador(a2,XOR(SR,b2),respuestas[0]);
+		listaRespuesta.add(respuestas[1]);
+		respuestas = sumador(a1,XOR(SR,b1),respuestas[0]);
+		listaRespuesta.add(respuestas[1]);
+		for(int i =listaRespuesta.size()-1; i>=0;i--) {
+			System.out.print(listaRespuesta.get(i));
+		}
+		return listaRespuesta;
+	}
+	public Boolean[] sumador(Boolean a,Boolean b, Boolean cIn) {
+		Boolean cOut = OR(AND(true,a,b,cIn),AND(true,a,b,NOT(cIn)),AND(true,a,NOT(b),cIn),AND(true,NOT(a),b,cIn));
+		Boolean R = AND(OR(false,a,b,cIn),OR(false,a,NOT(b),NOT(cIn)),OR(false,NOT(a),b,NOT(cIn)),OR(false,NOT(a),NOT(b),cIn));
+		return new Boolean[]{cOut,R};
+	}
 	public static void main(String []args) {
 		Compuertas asd = new Compuertas();
-		System.out.println(asd.NOT(true));
-		System.out.println(asd.NAND(true,true,true,true));
+		asd.Alu(false,true,false,true,true,false,false,true,false);
 	}
 }
