0f19c213ea74351a5f04d9e17ec5931d9c8ee9f1
Froyd Marchena <marchena.froyd23@gmail.com>
2019-11-24 21:21:12 -0600

    7 segmentos

--- Compuertas.java
+++ Compuertas.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import java.util.List;
 
 public class Compuertas {
 	public Boolean NOT(Boolean a) {
@@ -41,8 +40,43 @@ public class Compuertas {
 		Boolean R = AND(OR(false,a,b,cIn),OR(false,a,NOT(b),NOT(cIn)),OR(false,NOT(a),b,NOT(cIn)),OR(false,NOT(a),NOT(b),cIn));
 		return new Boolean[]{cOut,R};
 	}
+	
+	
+	public Boolean[] BinaryConvertDecimal(Boolean a,Boolean b,Boolean c,Boolean d) {
+		Boolean[] result = new Boolean[7];
+		result[0]=OR(OR(AND(NOT(a),NOT(b),NOT(c),NOT(d)),AND(NOT(a),NOT(b),c,NOT(d)),AND(NOT(a),NOT(b),c,d),AND(NOT(a),b,NOT(c),d)),OR(AND(NOT(a),b,c,NOT(d)),AND(NOT(a),b,c,d),AND(a,NOT(b),NOT(c),NOT(d)),AND(a,NOT(b),NOT(c),d)),false,false);
+		result[1]=OR(OR(AND(NOT(a),NOT(b),NOT(c),NOT(d)),AND(NOT(a),NOT(b),NOT(c),d),AND(NOT(a),NOT(b),c,NOT(d)),AND(NOT(a),NOT(b),c,d)),OR(AND(NOT(a),b,NOT(c),NOT(d)),AND(NOT(a),b,c,d),AND(a,NOT(b),NOT(c),NOT(d)),AND(a,NOT(b),NOT(c),d)),false,false);
+		result[2]=OR(OR(AND(NOT(a),NOT(b),NOT(c),NOT(d)),AND(NOT(a),NOT(b),NOT(c),d),AND(NOT(a),b,NOT(c),NOT(d)),AND(NOT(a),b,NOT(c),d)),OR(AND(NOT(a),b,c,NOT(d)),AND(NOT(a),b,c,d),AND(a,NOT(b),NOT(c),NOT(d)),AND(a,NOT(b),NOT(c),d)),false,false);
+		result[3]=OR(OR(AND(NOT(a),NOT(b),NOT(c),NOT(d)),AND(NOT(a),NOT(b),c,NOT(d)),AND(NOT(a),NOT(b),c,d),AND(NOT(a),b,NOT(c),d)),OR(AND(NOT(a),b,c,NOT(d)),AND(a,NOT(b),NOT(c),NOT(d)),AND(a,NOT(b),NOT(c),d),false),false,false);
+		result[4]=OR(OR(AND(NOT(a),NOT(b),NOT(c),NOT(d)),AND(NOT(a),NOT(b),c,NOT(d)),AND(NOT(a),b,c,NOT(d)),AND(a,NOT(b),NOT(c),NOT(d))),false,false,false);
+		result[5]=OR(OR(AND(NOT(a),NOT(b),NOT(c),NOT(d)),AND(NOT(a),b,NOT(c),NOT(d)),AND(NOT(a),b,NOT(c),d),AND(NOT(a),b,c,NOT(d))),OR(AND(a,NOT(b),NOT(c),NOT(d)),AND(a,NOT(b),NOT(c),d),false,false),false,false);
+		result[6]=OR(OR(AND(NOT(a),NOT(b),c,NOT(d)),AND(NOT(a),NOT(b),c,d),AND(NOT(a),b,NOT(c),NOT(d)),AND(NOT(a),b,NOT(c),d)),OR(AND(NOT(a),b,c,NOT(d)),AND(a,NOT(b),NOT(c),NOT(d)),AND(a,NOT(b),NOT(c),d),false),false,false);
+		return result;
+	}
+	
+	
 	public static void main(String []args) {
 		Compuertas asd = new Compuertas();
-		asd.Alu(false,true,false,true,true,false,false,true,false);
+		Boolean[] datos=asd.BinaryConvertDecimal(false, false, false, false);
+		System.out.println("Numero 0:"+datos[0]+" "+datos[1]+" "+datos[2]+" "+datos[3]+" "+datos[4]+" "+datos[5]+" "+datos[6]);
+		datos=asd.BinaryConvertDecimal(false, false, false, true);
+		System.out.println("Numero 1:"+datos[0]+" "+datos[1]+" "+datos[2]+" "+datos[3]+" "+datos[4]+" "+datos[5]+" "+datos[6]);
+		datos=asd.BinaryConvertDecimal(false, false,true, false);
+		System.out.println("Numero 2:"+datos[0]+" "+datos[1]+" "+datos[2]+" "+datos[3]+" "+datos[4]+" "+datos[5]+" "+datos[6]);
+		datos=asd.BinaryConvertDecimal(false, false, true, true);
+		System.out.println("Numero 3:"+datos[0]+" "+datos[1]+" "+datos[2]+" "+datos[3]+" "+datos[4]+" "+datos[5]+" "+datos[6]);
+		datos=asd.BinaryConvertDecimal(false, true, false, false);
+		System.out.println("Numero 4:"+datos[0]+" "+datos[1]+" "+datos[2]+" "+datos[3]+" "+datos[4]+" "+datos[5]+" "+datos[6]);
+		datos=asd.BinaryConvertDecimal(false, true, false, true);
+		System.out.println("Numero 5:"+datos[0]+" "+datos[1]+" "+datos[2]+" "+datos[3]+" "+datos[4]+" "+datos[5]+" "+datos[6]);
+		datos=asd.BinaryConvertDecimal(false, true, true, false);
+		System.out.println("Numero 6:"+datos[0]+" "+datos[1]+" "+datos[2]+" "+datos[3]+" "+datos[4]+" "+datos[5]+" "+datos[6]);
+		datos=asd.BinaryConvertDecimal(false, true, true, true);
+		System.out.println("Numero 7:"+datos[0]+" "+datos[1]+" "+datos[2]+" "+datos[3]+" "+datos[4]+" "+datos[5]+" "+datos[6]);
+		datos=asd.BinaryConvertDecimal(true, false, false, false);
+		System.out.println("Numero 8:"+datos[0]+" "+datos[1]+" "+datos[2]+" "+datos[3]+" "+datos[4]+" "+datos[5]+" "+datos[6]);
+		datos=asd.BinaryConvertDecimal(true, false, false, true);
+		System.out.println("Numero 9:"+datos[0]+" "+datos[1]+" "+datos[2]+" "+datos[3]+" "+datos[4]+" "+datos[5]+" "+datos[6]);
+		
 	}
 }
