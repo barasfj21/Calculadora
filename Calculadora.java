b5b9c804fa3de1446f79495ca2f91284c0f33d51
Froyd Marchena <marchena.froyd23@gmail.com>
2019-11-26 00:55:12 -0600

    new features

--- Calculadora.java
+++ Calculadora.java
@@ -13,11 +13,11 @@ import javax.swing.JLabel;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.awt.event.ActionEvent;
+import java.awt.Color;
 
 public class Calculadora extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField textField;
 	
 	//Lista con los numeros en boolean
 	Boolean[] cero = {false,false,false,false};
@@ -43,6 +43,39 @@ public class Calculadora extends JFrame {
 	
 	
 	Compuertas asd = new Compuertas();
+	
+	JPanel pnR0Num1 = new JPanel();
+	JPanel pnR5Num1 = new JPanel();
+	JPanel pnR6Num1 = new JPanel();
+	JPanel pnR1Num1 = new JPanel();
+	JPanel pnR2Num1 = new JPanel();
+	JPanel pnR4Num1 = new JPanel();
+	JPanel pnR3Num1 = new JPanel();
+	JPanel pnR3Num2 = new JPanel();
+	JPanel pnR2Num2 = new JPanel();
+	JPanel pnR4Num2 = new JPanel();
+	JPanel pnR6Num2 = new JPanel();
+	JPanel pnR5Num2 = new JPanel();
+	JPanel pnR1Num2 = new JPanel();
+	JPanel pnR0Num2 = new JPanel();
+	JPanel pnR3Num3 = new JPanel();
+	JPanel pnR2Num3 = new JPanel();
+	JPanel pnR4Num3 = new JPanel();
+	JPanel pnR6Num3 = new JPanel();
+	JPanel pnR5Num3 = new JPanel();
+	JPanel pnR1Num3 = new JPanel();
+	JPanel pnR0Num3 = new JPanel();
+	JPanel pnR5Num4 = new JPanel();
+	JPanel pnR4Num4 = new JPanel();
+	JPanel pnR3Num4 = new JPanel();
+	JPanel pnR2Num4 = new JPanel();
+	JPanel pnR6Num4 = new JPanel();
+	JPanel pnR1Num4 = new JPanel();
+	JPanel pnR0Num4 = new JPanel();
+	private final JPanel pnOperator = new JPanel();
+	private final JPanel pnOperador2 = new JPanel();
+	private final JPanel panel_1 = new JPanel();
+	private final JPanel panel_2 = new JPanel();
 	
 
 	/**
@@ -192,15 +225,6 @@ public class Calculadora extends JFrame {
 		button_11.setBounds(264, 111, 41, 31);
 		contentPane.add(button_11);
 		
-		textField = new JTextField();
-		textField.setBounds(251, 223, 74, 20);
-		contentPane.add(textField);
-		textField.setColumns(10);
-		
-		JLabel lblResultado = new JLabel("Result");
-		lblResultado.setBounds(275, 198, 49, 14);
-		contentPane.add(lblResultado);
-		
 		JButton button_12 = new JButton("1");
 		button_12.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -320,9 +344,265 @@ public class Calculadora extends JFrame {
 		contentPane.add(button_21);
 		
 		JButton btnClear = new JButton("Clear");
-		btnClear.setBounds(243, 273, 89, 23);
+		btnClear.setBounds(239, 165, 89, 23);
 		contentPane.add(btnClear);
+		
+		
+		pnR0Num1.setBackground(Color.RED);
+		pnR0Num1.setBounds(56, 215, 51, 10);
+		contentPane.add(pnR0Num1);
+		
+		
+		pnR5Num1.setBackground(Color.RED);
+		pnR5Num1.setBounds(41, 226, 10, 51);
+		contentPane.add(pnR5Num1);
+		
+		
+		pnR6Num1.setBackground(Color.RED);
+		pnR6Num1.setBounds(56, 279, 51, 10);
+		contentPane.add(pnR6Num1);
+		
+		
+		pnR1Num1.setBackground(Color.RED);
+		pnR1Num1.setBounds(111, 226, 10, 51);
+		contentPane.add(pnR1Num1);
+		
+		
+		pnR2Num1.setBackground(Color.RED);
+		pnR2Num1.setBounds(111, 289, 10, 51);
+		contentPane.add(pnR2Num1);
+		
+		
+		pnR4Num1.setBackground(Color.RED);
+		pnR4Num1.setBounds(41, 289, 10, 51);
+		contentPane.add(pnR4Num1);
+		
+		
+		pnR3Num1.setBackground(Color.RED);
+		pnR3Num1.setBounds(56, 340, 51, 10);
+		contentPane.add(pnR3Num1);
+		
+		
+		pnR3Num2.setBackground(Color.RED);
+		pnR3Num2.setBounds(240, 340, 51, 10);
+		contentPane.add(pnR3Num2);
+		
+		
+		pnR2Num2.setBackground(Color.RED);
+		pnR2Num2.setBounds(295, 289, 10, 51);
+		contentPane.add(pnR2Num2);
+		
+		
+		pnR4Num2.setBackground(Color.RED);
+		pnR4Num2.setBounds(225, 289, 10, 51);
+		contentPane.add(pnR4Num2);
+		
+		
+		pnR6Num2.setBackground(Color.RED);
+		pnR6Num2.setBounds(240, 279, 51, 10);
+		contentPane.add(pnR6Num2);
+		
+		
+		pnR5Num2.setBackground(Color.RED);
+		pnR5Num2.setBounds(225, 226, 10, 51);
+		contentPane.add(pnR5Num2);
+		
+		
+		pnR1Num2.setBackground(Color.RED);
+		pnR1Num2.setBounds(295, 226, 10, 51);
+		contentPane.add(pnR1Num2);
+		
+		
+		pnR0Num2.setBackground(Color.RED);
+		pnR0Num2.setBounds(240, 215, 51, 10);
+		contentPane.add(pnR0Num2);
+		
+		
+		pnR3Num3.setBackground(Color.RED);
+		pnR3Num3.setBounds(400, 340, 51, 10);
+		contentPane.add(pnR3Num3);
+		
+		
+		pnR2Num3.setBackground(Color.RED);
+		pnR2Num3.setBounds(455, 289, 10, 51);
+		contentPane.add(pnR2Num3);
+		
+		
+		pnR4Num3.setBackground(Color.RED);
+		pnR4Num3.setBounds(385, 289, 10, 51);
+		contentPane.add(pnR4Num3);
+		
+		
+		pnR6Num3.setBackground(Color.RED);
+		pnR6Num3.setBounds(400, 279, 51, 10);
+		contentPane.add(pnR6Num3);
+		
+		
+		pnR5Num3.setBackground(Color.RED);
+		pnR5Num3.setBounds(385, 226, 10, 51);
+		contentPane.add(pnR5Num3);
+		
+		
+		pnR1Num3.setBackground(Color.RED);
+		pnR1Num3.setBounds(455, 226, 10, 51);
+		contentPane.add(pnR1Num3);
+		
+		
+		pnR0Num3.setBackground(Color.RED);
+		pnR0Num3.setBounds(400, 215, 51, 10);
+		contentPane.add(pnR0Num3);
+		
+		
+		pnR5Num4.setBackground(Color.RED);
+		pnR5Num4.setBounds(491, 226, 10, 51);
+		contentPane.add(pnR5Num4);
+		
+		
+		pnR4Num4.setBackground(Color.RED);
+		pnR4Num4.setBounds(491, 289, 10, 51);
+		contentPane.add(pnR4Num4);
+		
+		
+		pnR3Num4.setBackground(Color.RED);
+		pnR3Num4.setBounds(506, 340, 51, 10);
+		contentPane.add(pnR3Num4);
+		
+		
+		pnR2Num4.setBackground(Color.RED);
+		pnR2Num4.setBounds(561, 289, 10, 51);
+		contentPane.add(pnR2Num4);
+		
+		
+		pnR6Num4.setBackground(Color.RED);
+		pnR6Num4.setBounds(506, 279, 51, 10);
+		contentPane.add(pnR6Num4);
+		
+		
+		pnR1Num4.setBackground(Color.RED);
+		pnR1Num4.setBounds(561, 226, 10, 51);
+		contentPane.add(pnR1Num4);
+		
+		
+		pnR0Num4.setBackground(Color.RED);
+		pnR0Num4.setBounds(506, 215, 51, 10);
+		contentPane.add(pnR0Num4);
+		pnOperator.setBackground(Color.LIGHT_GRAY);
+		pnOperator.setBounds(147, 279, 51, 10);
+		
+		contentPane.add(pnOperator);
+		pnOperador2.setBackground(Color.LIGHT_GRAY);
+		pnOperador2.setBounds(166, 261, 10, 51);
+		
+		contentPane.add(pnOperador2);
+		panel_1.setBackground(Color.LIGHT_GRAY);
+		panel_1.setBounds(322, 261, 51, 10);
+		
+		contentPane.add(panel_1);
+		panel_2.setBackground(Color.LIGHT_GRAY);
+		panel_2.setBounds(322, 302, 51, 10);
+		
+		contentPane.add(panel_2);
+		
+		loadNumber1(false, false, false, true);
+		loadNumber2(false, true, false, false);
+		loadNumber4(true, true, false, false);
 	}
+	private void initNumber1() {
+		
+		pnR0Num1.setVisible(false);
+		pnR1Num1.setVisible(false);
+		pnR2Num1.setVisible(false);
+		pnR3Num1.setVisible(false);
+		pnR4Num1.setVisible(false);
+		pnR5Num1.setVisible(false);
+		pnR6Num1.setVisible(false);
 	
-
+	}
+	private void initNumber2() {
+		
+		
+		pnR0Num2.setVisible(false);
+		pnR1Num2.setVisible(false);
+		pnR2Num2.setVisible(false);
+		pnR3Num2.setVisible(false);
+		pnR4Num2.setVisible(false);
+		pnR5Num2.setVisible(false);
+		pnR6Num2.setVisible(false);
+		pnR0Num3.setVisible(false);
+		pnR1Num3.setVisible(false);
+		pnR2Num3.setVisible(false);
+		pnR3Num3.setVisible(false);
+		pnR4Num3.setVisible(false);
+		pnR5Num3.setVisible(false);
+		pnR6Num3.setVisible(false);
+	}
+	private void initNumber3() {
+	
+		pnR0Num3.setVisible(false);
+		pnR1Num3.setVisible(false);
+		pnR2Num3.setVisible(false);
+		pnR3Num3.setVisible(false);
+		pnR4Num3.setVisible(false);
+		pnR5Num3.setVisible(false);
+		pnR6Num3.setVisible(false);
+	}
+	private void initNumber4() {
+		
+		pnR0Num4.setVisible(false);
+		pnR1Num4.setVisible(false);
+		pnR2Num4.setVisible(false);
+		pnR3Num4.setVisible(false);
+		pnR4Num4.setVisible(false);
+		pnR5Num4.setVisible(false);
+		pnR6Num4.setVisible(false);
+	}
+	public void loadNumber1(Boolean a,Boolean b,Boolean c,Boolean d) {
+		initNumber1();
+		Boolean[] data = asd.BinaryConvertDecimal(a, b, c, d);
+		pnR0Num1.setVisible(data[0]);
+		pnR1Num1.setVisible(data[1]);
+		pnR2Num1.setVisible(data[2]);
+		pnR3Num1.setVisible(data[3]);
+		pnR4Num1.setVisible(data[4]);
+		pnR5Num1.setVisible(data[5]);
+		pnR6Num1.setVisible(data[6]);
+		
+	}
+	public void loadNumber2(Boolean a,Boolean b,Boolean c,Boolean d) {
+		initNumber2();
+		Boolean[] data = asd.BinaryConvertDecimal(a, b, c, d);
+		pnR0Num2.setVisible(data[0]);
+		pnR1Num2.setVisible(data[1]);
+		pnR2Num2.setVisible(data[2]);
+		pnR3Num2.setVisible(data[3]);
+		pnR4Num2.setVisible(data[4]);
+		pnR5Num2.setVisible(data[5]);
+		pnR6Num2.setVisible(data[6]);
+		
+	}
+	public void loadNumber3(Boolean a,Boolean b,Boolean c,Boolean d) {
+		initNumber3();
+		Boolean[] data = asd.BinaryConvertDecimal(a, b, c, d);
+		pnR0Num3.setVisible(data[0]);
+		pnR1Num3.setVisible(data[1]);
+		pnR2Num3.setVisible(data[2]);
+		pnR3Num3.setVisible(data[3]);
+		pnR4Num3.setVisible(data[4]);
+		pnR5Num3.setVisible(data[5]);
+		pnR6Num3.setVisible(data[6]);
+		
+	}
+	public void loadNumber4(Boolean a,Boolean b,Boolean c,Boolean d) {
+		initNumber4();
+		Boolean[] data = asd.BinaryConvertDecimal(a, b, c, d);
+		pnR0Num4.setVisible(data[0]);
+		pnR1Num4.setVisible(data[1]);
+		pnR2Num4.setVisible(data[2]);
+		pnR3Num4.setVisible(data[3]);
+		pnR4Num4.setVisible(data[4]);
+		pnR5Num4.setVisible(data[5]);
+		pnR6Num4.setVisible(data[6]);
+		loadNumber3(false, false, false, data[7]);
+		
+	}
 }
--- Compuertas.java
+++ Compuertas.java
@@ -43,7 +43,7 @@ public class Compuertas {
 	
 	
 	public Boolean[] BinaryConvertDecimal(Boolean a,Boolean b,Boolean c,Boolean d) {
-		Boolean[] result = new Boolean[7];
+		Boolean[] result = new Boolean[8];
 		result[0]=OR(OR(AND(NOT(a),NOT(b),NOT(c),NOT(d)),AND(NOT(a),NOT(b),c,NOT(d)),AND(NOT(a),NOT(b),c,d),AND(NOT(a),b,NOT(c),d)),OR(AND(NOT(a),b,c,NOT(d)),AND(NOT(a),b,c,d),AND(a,NOT(b),NOT(c),NOT(d)),AND(a,NOT(b),NOT(c),d)),false,false);
 		result[1]=OR(OR(AND(NOT(a),NOT(b),NOT(c),NOT(d)),AND(NOT(a),NOT(b),NOT(c),d),AND(NOT(a),NOT(b),c,NOT(d)),AND(NOT(a),NOT(b),c,d)),OR(AND(NOT(a),b,NOT(c),NOT(d)),AND(NOT(a),b,c,d),AND(a,NOT(b),NOT(c),NOT(d)),AND(a,NOT(b),NOT(c),d)),false,false);
 		result[2]=OR(OR(AND(NOT(a),NOT(b),NOT(c),NOT(d)),AND(NOT(a),NOT(b),NOT(c),d),AND(NOT(a),b,NOT(c),NOT(d)),AND(NOT(a),b,NOT(c),d)),OR(AND(NOT(a),b,c,NOT(d)),AND(NOT(a),b,c,d),AND(a,NOT(b),NOT(c),NOT(d)),AND(a,NOT(b),NOT(c),d)),false,false);
@@ -51,32 +51,22 @@ public class Compuertas {
 		result[4]=OR(OR(AND(NOT(a),NOT(b),NOT(c),NOT(d)),AND(NOT(a),NOT(b),c,NOT(d)),AND(NOT(a),b,c,NOT(d)),AND(a,NOT(b),NOT(c),NOT(d))),false,false,false);
 		result[5]=OR(OR(AND(NOT(a),NOT(b),NOT(c),NOT(d)),AND(NOT(a),b,NOT(c),NOT(d)),AND(NOT(a),b,NOT(c),d),AND(NOT(a),b,c,NOT(d))),OR(AND(a,NOT(b),NOT(c),NOT(d)),AND(a,NOT(b),NOT(c),d),false,false),false,false);
 		result[6]=OR(OR(AND(NOT(a),NOT(b),c,NOT(d)),AND(NOT(a),NOT(b),c,d),AND(NOT(a),b,NOT(c),NOT(d)),AND(NOT(a),b,NOT(c),d)),OR(AND(NOT(a),b,c,NOT(d)),AND(a,NOT(b),NOT(c),NOT(d)),AND(a,NOT(b),NOT(c),d),false),false,false);
+		result[7] = false;
+		if(result[0]==false && result[1]==false && result[2]==false && result[3]==false && result[4]==false&&result[5]==false && result[6]==false) {
+			ArrayList<Boolean> rs = Alu(a, b, c, d, true, false, false,false , true);
+			rs = Alu(rs.get(0),rs.get(1) , rs.get(2), rs.get(3), true, true, false, false ,true);
+			result = BinaryConvertDecimal(rs.get(0),rs.get(1) , rs.get(2), rs.get(3));
+			result[7] = true;
+		}
 		return result;
 	}
 	
 	
 	public static void main(String []args) {
 		Compuertas asd = new Compuertas();
-		Boolean[] datos=asd.BinaryConvertDecimal(false, false, false, false);
-		System.out.println("Numero 0:"+datos[0]+" "+datos[1]+" "+datos[2]+" "+datos[3]+" "+datos[4]+" "+datos[5]+" "+datos[6]);
-		datos=asd.BinaryConvertDecimal(false, false, false, true);
-		System.out.println("Numero 1:"+datos[0]+" "+datos[1]+" "+datos[2]+" "+datos[3]+" "+datos[4]+" "+datos[5]+" "+datos[6]);
-		datos=asd.BinaryConvertDecimal(false, false,true, false);
-		System.out.println("Numero 2:"+datos[0]+" "+datos[1]+" "+datos[2]+" "+datos[3]+" "+datos[4]+" "+datos[5]+" "+datos[6]);
-		datos=asd.BinaryConvertDecimal(false, false, true, true);
-		System.out.println("Numero 3:"+datos[0]+" "+datos[1]+" "+datos[2]+" "+datos[3]+" "+datos[4]+" "+datos[5]+" "+datos[6]);
-		datos=asd.BinaryConvertDecimal(false, true, false, false);
-		System.out.println("Numero 4:"+datos[0]+" "+datos[1]+" "+datos[2]+" "+datos[3]+" "+datos[4]+" "+datos[5]+" "+datos[6]);
-		datos=asd.BinaryConvertDecimal(false, true, false, true);
-		System.out.println("Numero 5:"+datos[0]+" "+datos[1]+" "+datos[2]+" "+datos[3]+" "+datos[4]+" "+datos[5]+" "+datos[6]);
-		datos=asd.BinaryConvertDecimal(false, true, true, false);
-		System.out.println("Numero 6:"+datos[0]+" "+datos[1]+" "+datos[2]+" "+datos[3]+" "+datos[4]+" "+datos[5]+" "+datos[6]);
-		datos=asd.BinaryConvertDecimal(false, true, true, true);
-		System.out.println("Numero 7:"+datos[0]+" "+datos[1]+" "+datos[2]+" "+datos[3]+" "+datos[4]+" "+datos[5]+" "+datos[6]);
-		datos=asd.BinaryConvertDecimal(true, false, false, false);
-		System.out.println("Numero 8:"+datos[0]+" "+datos[1]+" "+datos[2]+" "+datos[3]+" "+datos[4]+" "+datos[5]+" "+datos[6]);
-		datos=asd.BinaryConvertDecimal(true, false, false, true);
-		System.out.println("Numero 9:"+datos[0]+" "+datos[1]+" "+datos[2]+" "+datos[3]+" "+datos[4]+" "+datos[5]+" "+datos[6]);
+		Boolean[] datos=asd.BinaryConvertDecimal(true,false,true,false);
+		System.out.println("Numero 0:"+datos[0]+" "+datos[1]+" "+datos[2]+" "+datos[3]+" "+datos[4]+" "+datos[5]+" "+datos[6]+" "+datos[7]);
+		
 		
 	}
 }
